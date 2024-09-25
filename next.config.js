3caa39303939333254c1124141389a1d196a3777
addreeh <adrianpinohidalgo@gmail.com>
2024-09-25 12:43:09 +0200

    pwa

---
+++ next.config.js
@@ -0,0 +1,21 @@
+// next.config.js
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      { hostname: 'lh3.googleusercontent.com' }
+    ]
+  }
+}
+
+module.exports = withPWA(nextConfig)
